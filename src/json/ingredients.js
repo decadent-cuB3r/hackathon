f79668be2651a49e4a17c4c84ab68952d7d4d14b
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2021-05-15 02:39:16 +0800

    json

---
+++ src/json/ingredients.js
@@ -0,0 +1,44 @@
+[
+    {
+        "id": "chickenLeg",
+        "name": "雞腿",
+        "image": "https://s3-ap-southeast-1.amazonaws.com/zhonglingdev/resource/goods/item_M3060-1.jpg",
+        "quantifer": "隻"
+    },
+    {
+        "id": "gingerSlice",
+        "name": "薑黃",
+        "image": "https://www.lamabeibei.com/uploads/allimg/190125/17-1Z12510434IW.jpg",
+        "quantifer": "片"
+    },
+    {
+        "id": "garlic",
+        "name": "蒜",
+        "image": "https://i.epochtimes.com/assets/uploads/2015/01/1408140542492525-600x400.jpg",
+        "quantifer": "瓣"
+    },
+    {
+        "id": "sesameOil",
+        "name": "麻油",
+        "image": "https://www.great-oil.com.tw/webadmin/editor/attached/image/20180519/2018051912480107107.jpg",
+        "quantifer": "ml"
+    },
+    {
+        "id": "sugar",
+        "name": "糖",
+        "image": "https://www.getldi.com/wp-content/uploads/2017/09/bigstock-Bowl-and-scoop-with-white-sand-180259339.jpg",
+        "quantifer": "tea spoon"
+    },
+    {
+        "id": "soySauce",
+        "name": "醬油",
+        "image": "https://www.hollandshop24.com/media/image/product/1454/lg/kikkoman-soy-sauce.jpg",
+        "quantifer": "ml"
+    },
+    {
+        "id": "riceWine",
+        "name": "米酒",
+        "image": "",
+        "quantifer": ""
+    }
+]
